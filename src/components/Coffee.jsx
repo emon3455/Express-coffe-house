a8deb53e345998a4e89ad3ee405e0b36950f092c
Emon <emon.mhk69@gmail.com>
2023-05-08 08:02:47 +0600

    update done

--- src/components/Coffee.jsx
+++ src/components/Coffee.jsx
@@ -1,9 +1,52 @@
 /* eslint-disable react/prop-types */
-
+import Swal from 'sweetalert2'
 
 const Coffee = ({ coffe }) => {
 
-    const { name, chef, suplier, photo } = coffe;
+    const { _id, name, chef, suplier, photo } = coffe;
+
+    const handleDelete = (id)=>{
+        console.log(id);
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+
+            if (result.isConfirmed) {
+            
+                fetch(`http://localhost:5000/coffes/${id}`,{
+                    method:"DELETE"
+                })
+                .then(res=> res.json())
+                .then(data=> {
+                    console.log(data);
+                    if(data.deletedCount>0){
+                        Swal.fire(
+                            'Deleted!',
+                            'Your coffe has been deleted.',
+                            'success'
+                        );
+                    }
+                    else{
+                        Swal.fire({
+                            title: 'Error!',
+                            text: 'Something went wrong',
+                            icon: 'error',
+                            confirmButtonText: 'ok'
+                          }) 
+                    }
+                })
+            }
+          })
+
+       
+    }
 
     return (
         <div className="card card-side gap-10 bg-base-100 shadow-2xl p-2">
@@ -17,7 +60,7 @@ const Coffee = ({ coffe }) => {
                 <div className="btn-group btn-group-vertical space-y-1 w-1/3">
                     <button className="btn btn-active">View</button>
                     <button className="btn">Update</button>
-                    <button className="btn btn-warning"> X </button>
+                    <button onClick={()=>handleDelete(_id)} className="btn btn-warning"> X </button>
                 </div>
             </div>
         </div>
